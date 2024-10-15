37e1c9f2840fca43588c03ac9cdc033c8d7fbe0f
truongsut21 <truongsut21@github.com>
2024-10-15 14:03:17 +0700

    init: user schema

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,42 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest'
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'react',
+    'prettier'
+  ],
   extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended' // Kết hợp Prettier với ESLint
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    browser: true, // Hỗ trợ môi trường browser
+    es2021: true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': ['error'], // Sử dụng Prettier để kiểm tra format
+    'react/prop-types': 'off', // Tắt kiểm tra prop-types trong React
+    'no-unused-vars': 'warn', // Cảnh báo nếu có biến không sử dụng
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   },
+  settings: {
+    react: {
+      version: 'detect' // Tự động phát hiện phiên bản React
+    }
+  }
 };
